fe2371a3553dc82a1a188a466a4ff05ca2049076
Dominik Diesch <github.dominik@dominik.homeunix.org>
2014-05-02 00:47:47 +0200

    Comments added; changed default root and fixed a typo in a property key

--- src/de/kandid/junit/TestCase.java
+++ src/de/kandid/junit/TestCase.java
@@ -26,20 +26,48 @@ import java.util.Properties;
 import junit.framework.TestResult;
 
 /**
- *
- *
- * @version $Rev:$
+ * A TestCase that adds some logic for the handling of temporary directories. Its main purpose
+ * is to control where and how long temporary files and directories live. The properties will be
+ * read from a file located at <tt>$HOME/.config/de.kandid/general.properties</tt>.<p/>
+ * The following keys are recognized:
+ * <table><thead><tr>
+ * 	<th>Property</th><th>Description</th><th>Default</th>
+ * </tr></thead><tbody><tr>
+ * 	<td>{@code de.kandid.junit.tmp.dir}</td>
+ * 	<td>the location where to create all temporary files and and directories</td>
+ * 	<td>{@code build/tmp/unittest}</td>
+ * </tr><tr>
+ * 	<td>{@code de.kandid.junit.tmp.dir.remove}</td>
+ * 	<td> a {@link TmpDirDeletePolicy} as a String controlling when to delete the files created for this TestCase</td>
+ * 	<td>{@code passed}</td>
+ * </tr></tbody></table>
+ * Both values are global to all TestCases descending from this class.
  */
 public abstract class TestCase extends junit.framework.TestCase {
 
+	/**
+	 * This exception will be thrown if an attempt is made to delete a file outside
+	 * of the designated unit test zone.
+	 */
 	public static class NotAUnitTestTempDirException extends RuntimeException {
-		public NotAUnitTestTempDirException(File dir) {
+		private NotAUnitTestTempDirException(File dir) {
 			super("Not a unit test temporary directory: " + dir);
 		}
 	}
 
-	public enum TmpDirDeletePolicy {always, passed, never};
+	public enum TmpDirDeletePolicy {
+		/** Always delete the files after the test has finished */
+		always,
+		/** Only delete the temporary files when the test has finished successfully */
+		passed,
+		/** Never delete the test files */
+		never
+	};
 
+	/**
+	 * Constructs a test case with the given name.
+	 * @param name the name of this test case
+	 */
 	public TestCase(String name) {
 		super(name);
 		Properties props = new Properties(System.getProperties());
@@ -48,10 +76,13 @@ public abstract class TestCase extends junit.framework.TestCase {
 		   props.load(new FileInputStream(_properties));
 		} catch (IOException e) {
 		}
-		_tempRoot = new File(props.getProperty("de.kandid.junit.tmp.dir", "out/unittest"));
-		_tmpDirDeletePolicy = TmpDirDeletePolicy.valueOf(props.getProperty("de.kandid.junit.tmpdir.remove", "passed"));
+		_tempRoot = new File(props.getProperty("de.kandid.junit.tmp.dir", "build/tmp/unittest"));
+		_tmpDirDeletePolicy = TmpDirDeletePolicy.valueOf(props.getProperty("de.kandid.junit.tmp.dir.remove", "passed"));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void run(TestResult result) {
 		super.run(result);
@@ -62,6 +93,13 @@ public abstract class TestCase extends junit.framework.TestCase {
 		}
 	}
 
+	/**
+	 * Creates a unique directory inside the unit test root. Its name starts with the name
+	 * of the test and will be deleted at the end of the test according the policy given
+	 * in the properties.
+	 * @return the newly created directory
+	 * @throws IOException if the directory could not be created
+	 */
 	public File createTempDir() throws IOException {
 		ensureTempRoot();
 		File dir = Files.createTempDirectory(_tempRoot.toPath(), getName()).toFile();
@@ -69,6 +107,13 @@ public abstract class TestCase extends junit.framework.TestCase {
 		return dir;
 	}
 
+	/**
+	 * Creates a unique file inside the unit test root. Its name starts with the name
+	 * of the test and will be deleted at the end of the test according the policy given
+	 * in the properties.
+	 * @return the newly created file
+	 * @throws IOException if the directory could not be created
+	 */
 	public File createTempFile() throws IOException {
 		ensureTempRoot();
 		File file = File.createTempFile(getName(), null, _tempRoot);
@@ -76,7 +121,14 @@ public abstract class TestCase extends junit.framework.TestCase {
 		return file;
 	}
 
-
+	/**
+	 * Deletes the file or directory (recursively) denoted by {@code file}. An exception
+	 * will be thrown if the the file is not inside the unit test directory.
+	 * <em>Note</em>: the test for containment is a little sloppy. Paths with <tt>../</tt>
+	 * will fool the safety check!
+	 * @param file the file to delete
+	 * @throws NotAUnitTestTempDirException if the file is outside of the unit test dir
+	 */
 	public void deleteTempFile(File file) {
 		if (!file.toString().startsWith(_tempRoot.toString()))
 			throw new NotAUnitTestTempDirException(file);
@@ -96,8 +148,19 @@ public abstract class TestCase extends junit.framework.TestCase {
 		}
 	}
 
-	public static final File _properties = new File(System.getProperty("user.home") + "/.config/kandid.de/general.properties");
+	/**
+	 * The path of the properties controlling the behaviour of this class
+	 */
+	public static final File _properties = new File(System.getProperty("user.home") + "/.config/de.kandid/general.properties");
+
+	/**
+	 * The (configurable) root of the created test files.
+	 */
 	public final File _tempRoot;// = new File(System.getProperty("junit.tmp.dir", "out/unittest"));
+
+	/**
+	 * The policy how to handle the created files and directories after the test has finished.
+	 */
 	public TmpDirDeletePolicy _tmpDirDeletePolicy = TmpDirDeletePolicy.passed;
 	private final HashSet<File> _tempFiles = new HashSet<>();
 }
